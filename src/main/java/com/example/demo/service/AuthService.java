cfb6b6cb12ea38097827d1c719ec4ea37dcb8463
bala-amypo <bala@amypo.in>
2025-12-26 09:09:30 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface AuthService {
 
     AuthResponse register(RegisterRequest request);
